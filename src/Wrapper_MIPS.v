1d8965ebc9e0b74a98e1ae68ecd5bed9df8b4b4b
jquinonezb <89792961+jquinonezb@users.noreply.github.com>
2021-12-16 20:56:23 -0600

    Update of MIPS Wrapper
    T7

--- src/Wrapper_MIPS.v
+++ src/Wrapper_MIPS.v
@@ -1,17 +1,16 @@
 module Wrapper_MIPS
 (
 	input MAX10_CLK1_50,
-	input [9:9] SW,
-	output [7:0] LEDR
+	input [9:0] SW,
+	output [9:0] LEDR
 );
 
 wire clk, clk_hz;
 
 PLL	PLL_inst (.inclk0( MAX10_CLK1_50 ), .c0(clk));
 
-cont_1s_RCO Clock(.mclk(clk), .reset(SW), .RCO(clk_hz));
+cont_1s_RCO Clock(.mclk(clk), .reset(SW[9]), .RCO(clk_hz));
 
-Data_Path MIPS(.clk(clk_hz), .reset(SW), .GPIO_o(LEDR));
-
-
-endmodule 
+Data_Path MIPS(.clk(clk_hz), .reset(SW[9]), .GPIO_i(SW[7:0]), .clk_signal(LEDR[9]), .GPIO_o(LEDR[7:0]));
+endmodule
+ 
